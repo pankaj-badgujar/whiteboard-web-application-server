9c8941eacb7a30f0b7462a6257a4bedad82f77ce
pankajbadgujar <badgujar.p@husky.neu.edu>
2019-07-27 06:27:25 -0400

    added process.env.PORT statement
    added process.env.PORT statement so that the app runs on heroku which assigns it its own port as well as runs on local machine because it is ORed with 3000

--- server.js
+++ server.js
@@ -11,4 +11,4 @@ app.use(bodyParser.json());
 const studentService = require('./services/student.service.server')(app);
 const questionService = require('./services/question.service.server')(app);
 const answerService = require('./services/answer.service.server')(app);
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
