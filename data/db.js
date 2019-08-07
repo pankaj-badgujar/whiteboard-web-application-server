df965799241a81b5c7a3252d34a783269e68acf3
pankajbadgujar <badgujar.p@husky.neu.edu>
2019-08-07 09:17:01 -0500

    added mLab mongoDB credentials

--- data/db.js
+++ data/db.js
@@ -1,9 +1,14 @@
 module.exports = function () {
     const mongoose = require('mongoose');
-    const databaseName = 'whiteboard';
-    const server = 'localhost';
-    const port='27017';
-    mongoose.connect(`mongodb://${server}:${port}/${databaseName}`, {useNewUrlParser: true});
+
+    // const databaseName = 'whiteboard';
+    // const server = 'localhost';
+    // const port='27017';
+
+      // mongoose.connect(`mongodb://${server}:${port}/${databaseName}`, {useNewUrlParser: true});
+
+    mongoose.connect('mongodb://wbdv-su19-pankajBadgujar:wbdv-su19-pankajBadgujar@ds259787.mlab.com:59787/' +
+        'heroku_9gjxhttz',{useNewUrlParser: true});
 };
 
 
